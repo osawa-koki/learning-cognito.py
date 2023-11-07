0748496077348312512d4a2435c246094b54b8a5
osawa-koki <koki@osawa.dev>
2023-11-07 22:39:56 +0900

    poetry run autopep8 --in-place --recursive --aggressive --aggressive ./app/

--- app/models/confirm_forgot_password.py
+++ app/models/confirm_forgot_password.py
@@ -4,4 +4,7 @@ from pydantic import BaseModel, EmailStr, Field
 class ConfirmForgotPasswordModel(BaseModel):
     email: EmailStr
     code: str = Field(..., min_length=6, max_length=6)
-    password: str = Field(..., pattern="^[\\S]+.*[\\S]+$", min_length=8, max_length=16)
+    password: str = Field(...,
+                          pattern="^[\\S]+.*[\\S]+$",
+                          min_length=8,
+                          max_length=16)
