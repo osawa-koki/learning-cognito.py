072817d7ef236303d2ee2c6e5b19a56d8bf1be68
osawa-koki <koki@osawa.dev>
2023-11-07 22:15:25 +0900

    poetry run autopep8 --in-place --recursive --aggressive --aggressive ./app/

--- app/models/change_password.py
+++ app/models/change_password.py
@@ -3,10 +3,10 @@ from pydantic import BaseModel, Field
 
 class ChangePasswordModel(BaseModel):
     previous_password: str = Field(...,
-                          pattern="^[\\S]+.*[\\S]+$",
-                          min_length=8,
-                          max_length=16)
+                                   pattern="^[\\S]+.*[\\S]+$",
+                                   min_length=8,
+                                   max_length=16)
     proposed_password: str = Field(...,
-                        pattern="^[\\S]+.*[\\S]+$",
-                        min_length=8,
-                        max_length=16)
+                                   pattern="^[\\S]+.*[\\S]+$",
+                                   min_length=8,
+                                   max_length=16)
