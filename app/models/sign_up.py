69b2364ada0415cb8377a203f9f5d3c11d6d7040
osawa-koki <koki@osawa.dev>
2023-11-07 15:02:34 +0900

    poetry run autopep8 --in-place --recursive --aggressive --aggressive ./app/

--- app/models/sign_up.py
+++ app/models/sign_up.py
@@ -4,4 +4,7 @@ from pydantic import BaseModel, EmailStr, Field
 class SignUpModel(BaseModel):
     name: str = Field(..., min_length=3, max_length=16)
     email: EmailStr
-    password: str = Field(..., pattern="^[\S]+.*[\S]+$", min_length=8, max_length=16)
+    password: str = Field(...,
+                          pattern="^[\\S]+.*[\\S]+$",
+                          min_length=8,
+                          max_length=16)
